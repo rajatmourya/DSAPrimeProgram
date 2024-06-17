e1ad31092ddb196c92651684f6849f3b0033f8fc
Rajat Mourya <rm24042000@gmail.com>
2024-06-17 23:55:35 +0530

    Create Stack Using LinkedList

--- DataStructure/Stack/StackMainClass.java
+++ DataStructure/Stack/StackMainClass.java
@@ -6,7 +6,7 @@ public class StackMainClass {
     public static void main(String[] args) {
 
 //        callTOStackUsingArray();
-        callTOStackUsingLinkedList();
+        callTOStackUsingLinkedList();k
 
     }
 
